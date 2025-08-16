cf94af3551dc4db5d984561d1e367b9594838e04
Kashif-509 <kashifash509@gmail.com>
2025-08-17 00:19:39 +0500

    dynamdsasdsdfic

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,7 +1,7 @@
 // pages/blog/[slug].js
 import Head from "next/head";
 import { getBaseUrl } from "../../lib/url";
-import { buildArticleMeta } from "../lib/Meta";
+import { buildArticleMeta } from "../lib/siteMeta";
 
 export default function BlogArticle({ meta }) {
   return (
