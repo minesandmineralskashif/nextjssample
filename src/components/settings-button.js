9aec9843802d41bbeacf7d63be382894d4227213
Kashif-509 <kashifash509@gmail.com>
2025-09-02 23:44:04 +0500

    adjustmentssdw

--- src/components/settings-button.js
+++ src/components/settings-button.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Fab, Tooltip } from "@mui/material";
-// import { adjustments as AdjustmentsIcon } from "../icons/adjustments";
+import { adjustments as AdjustmentsIcon } from "../icons/adjustments";
 import { SettingsDrawer } from "./settings-drawer";
 
 export function SettingsButton() {
